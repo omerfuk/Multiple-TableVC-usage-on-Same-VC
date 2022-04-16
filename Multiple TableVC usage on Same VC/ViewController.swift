4afb3249515b56b348337641d0ea02230e7dea50
Omer Faruk Kilicaslan <omerrr_2001@hotmail.com>
2022-04-16 16:48:55 +0300

    first commit

--- Multiple TableVC usage on Same VC/ViewController.swift
+++ Multiple TableVC usage on Same VC/ViewController.swift
@@ -9,11 +9,76 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableViewSehirler: UITableView!
+    @IBOutlet weak var tableViewUlkeler: UITableView!
+    
+    var ulkeler = [String]()
+    var sehirler = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        ulkeler = ["Türkiye", "Almanya", "Japonya", "Rusya", "İtalya"]
+        sehirler = ["İstanbul", "Ankara", "Bursa", "Adana"]
+        
+        tableViewUlkeler.delegate = self
+        tableViewSehirler.delegate = self
+        
+        tableViewUlkeler.dataSource = self
+        tableViewSehirler.dataSource = self
     }
 
 
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource{
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        var veriSayisi:Int?
+        
+        if tableView == tableViewUlkeler {
+            veriSayisi = ulkeler.count
+        }
+        
+        if tableView == tableViewSehirler {
+            veriSayisi = sehirler.count
+        }
+        
+        return veriSayisi!
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell:UITableViewCell?
+        
+        if tableView == tableViewUlkeler {
+            cell = tableView.dequeueReusableCell(withIdentifier: "ulkelerHucre", for: indexPath)
+            cell?.textLabel?.text = ulkeler[indexPath.row]
+        }
+        
+        if tableView == tableViewSehirler {
+            cell = tableView.dequeueReusableCell(withIdentifier: "sehirlerHucre", for: indexPath)
+            cell?.textLabel?.text = sehirler[indexPath.row]
+        }
+        
+        return cell!
+        
+        
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if tableView == tableViewUlkeler {
+            print("Seçilen Ülke : \(ulkeler[indexPath.row])")
+            
+        }
+        
+        if tableView == tableViewSehirler {
+            print("Seçilen Sehir : \(sehirler[indexPath.row])")
+        }
+    }
+    
 }
 
